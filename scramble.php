e0b5cfc779276e5676f798c5ad9f1b87616408e7
Ammar Faizi <ammarfaizi2@gmail.com>
2017-12-22 16:39:07 +0700

    Fix script

--- scramble.php
+++ scramble.php
@@ -1,2 +1,9 @@
 <?php
 
+function scramble($str, $arr){
+  	$pure = $str xor $str = str_split($str);
+  	array_walk($arr, function ($q,$in) use (&$str, $pure) {
+  		$str[$q] = $pure[$in];
+  	});
+	return implode($str,"");
+}
